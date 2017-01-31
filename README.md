912c1468d18115fc55e27dc3c37b370bbaf0570f
Robert McBryde <rob@yavi.co>
2017-01-31 10:59:59 +0000

    Completion of the Master/Detail section

--- README.md
+++ README.md
@@ -164,6 +164,3 @@ A custom reporter (see `protractor.config.js`) generates a  `./_test-output/prot
 which is easier to read; this file is excluded from source control.
 
 Shut it down manually with `Ctrl-C`.
-
-[travis-badge]: https://travis-ci.org/angular/quickstart.svg?branch=master
-[travis-badge-url]: https://travis-ci.org/angular/quickstart
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,107 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: `
-          <h1>{{title}}</h1> 
-          <h2>{{hero.name}} details!</h2>
-          <div><label>id: </label>{{hero.id}}</div>
-          <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name"/>
-          </div>
-            `,
-})
-export class AppComponent  {
-  title = 'Rob Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Phil Dunphys'
-  };
-}
-
 export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 10, name: 'Mr Nice' },
+  { id: 11, name: 'Narco' },
+  { id: 12, name: 'Bombasto' },
+  { id: 13, name: 'Celeritas' },
+  { id: 14, name: 'Magneto' },
+  { id: 15, name: 'RubberMan' },
+  { id: 16, name: 'Dynamo' },
+  { id: 17, name: 'Dr IQ' },
+  { id: 18, name: 'Magma' },
+  { id: 19, name: 'Phil Dunphy' },
+  { id: 20, name: 'Le Shadow' }
+];
+
+@Component({
+  selector: 'my-app',
+  template: `
+          <h1>{{title}}</h1> 
+          
+          <h2>My Heroes</h2>
+          <ul class="heroes">
+            <li *ngFor="let hero of heroes" (click)="onSelect(hero)"
+            [class.selected] = "hero === selectedHero">
+              <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+          </ul>
+
+          <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div>
+              <label>name: </label>
+              <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+            </div>
+          </div>
+            `,
+    styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
+})
+
+export class AppComponent {
+  title = 'Rob Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+}
+
+
